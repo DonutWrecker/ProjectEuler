bc627dc96499f81ae554150b5a9d3019629aea44
DonutWrecker <donutwrecker@gmail.com>
2022-10-01 15:16:03 +0300

    Problem 2: Solved

--- P2_EvenFibonacciNumbers/Program.cs
+++ P2_EvenFibonacciNumbers/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿uint fib1 = 1;
+uint fib2 = 2;
+uint sumEven = fib2;
+
+while (fib1 <= 4_000_000 && fib2 <= 4_000_000)
+{
+    fib1 += fib2;
+    fib2 += fib1;
+
+    if (fib1 % 2 == 0) sumEven += fib1;
+    if (fib2 % 2 == 0) sumEven += fib2;
+}
+
+Console.WriteLine($"The sum of even-valued Fibonacci sequence terms whose values do not exceed 4.000.000: {sumEven}");
