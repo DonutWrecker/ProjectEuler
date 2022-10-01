5fbb04f3aa985eb27763e0938b45372c16a1648a
DonutWrecker <donutwrecker@gmail.com>
2022-10-01 16:31:17 +0300

    Problem 3: Solved

--- P3_LargestPrimeFactor/Program.cs
+++ P3_LargestPrimeFactor/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ulong numberOriginal = 600_851_475_143;
+ulong number = numberOriginal;
+ulong largestPrimeFactor = 1;
+
+for (ulong factor = 3UL; factor <= number; factor += 2)
+{
+    if (number % factor == 0)
+    {
+        number = DivideUntilNoMore(number, factor);
+        largestPrimeFactor = factor;
+    }
+}
+
+Console.WriteLine($"The largest prime factor of the number {numberOriginal}: {largestPrimeFactor}");
+
+static ulong DivideUntilNoMore(ulong number, ulong factor)
+{
+    while (number % factor == 0)
+    {
+        number /= factor;
+    }
+    
+    return number;
+}
