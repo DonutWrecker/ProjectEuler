56c9601b5d547a47a98c87063bc474f55d536aa6
DonutWrecker <donutwrecker@gmail.com>
2022-10-01 15:02:31 +0300

    Problem 1: Solved

--- MultiplesOf3or5/Program.cs
+++ MultiplesOf3or5/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int sum = 0;
+
+for (int i = 3; i < 1000; ++i)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        sum += i;
+    }
+}
+
+Console.WriteLine($"The sum of all the multiples of 3 or 5 below 1000: {sum}");
