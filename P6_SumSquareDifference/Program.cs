928ae320a265d5bad867b93499a2a0ff901c50bc
DonutWrecker <donutwrecker@gmail.com>
2022-10-02 13:51:18 +0300

    Problem 6: Started, Solved.

---
+++ P6_SumSquareDifference/Program.cs
@@ -0,0 +1,7 @@
+﻿ulong firstXnumbers = 100;
+ulong difference = ComputeSquareOfTheSum(firstXnumbers) - ComputeSumOfTheSquares(firstXnumbers);
+Console.WriteLine("The difference between the sum of the squares of the " +
+    $"first {firstXnumbers} natural numbers and the square of the sum: {difference}");
+
+static ulong ComputeSumOfTheSquares(ulong firstXnumbers) => firstXnumbers * (firstXnumbers + 1) * (2 * firstXnumbers + 1) / 6;
+static ulong ComputeSquareOfTheSum(ulong firstXnumbers) => (ulong)Math.Pow((double)(firstXnumbers * (firstXnumbers + 1) / 2), 2.0);
