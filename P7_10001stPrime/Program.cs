4da0cd7f4b203f2fc66a695b756ee39035f4d883
DonutWrecker <donutwrecker@gmail.com>
2022-10-02 14:22:16 +0300

    Problem 7: Started, Solved.

---
+++ P7_10001stPrime/Program.cs
@@ -0,0 +1,19 @@
+﻿int xThPrime = 10_001;
+ulong[] primes = new ulong[xThPrime];
+primes[0] = 2;
+
+for (ulong num = 3; primes[^1] == 0; ++num)
+{
+    for (int i = 0; i < xThPrime; ++i)
+    {
+        if (primes[i] == 0)
+        {
+            primes[i] = num;
+            break;
+        }
+
+        if (num % primes[i] == 0) break;
+    }
+}
+
+Console.WriteLine($"The {xThPrime}st prime number: {primes[^1]}");
