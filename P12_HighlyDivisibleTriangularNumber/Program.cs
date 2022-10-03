6d16fd3659125c4c7cd3e8493f3fdd2c1671e038
DonutWrecker <donutwrecker@gmail.com>
2022-10-03 14:10:34 +0300

    Problem 12: Solved (Slow)

--- P12_HighlyDivisibleTriangularNumber/Program.cs
+++ P12_HighlyDivisibleTriangularNumber/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿ulong numDivisors = 500;
+ulong number = 0;
+
+for (ulong index = 1, divisorCount = 0; divisorCount < numDivisors; ++index)
+{
+    number = GetTriangularNumber(index);
+    divisorCount = CountDivisors(number);
+}
+
+Console.WriteLine($"The value of the first triangle number to have over {numDivisors} divisors: {number}");
+
+static ulong GetTriangularNumber(ulong index) => index * (index + 1) / 2;
+
+static ulong CountDivisors(ulong number)
+{
+    ulong count = 0;
+    for (ulong divisor = 1; divisor <= number; ++divisor)
+    {
+        if (number % divisor == 0)
+        {
+            ++count;
+        }
+    }
+
+    return count;
+}
